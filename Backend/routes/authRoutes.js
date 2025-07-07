399150019e44310b16dee60fd80681948ce6663e
pavithran-c <pavithran71271@gmail.com>
2025-07-07 20:27:06 +0530

    asdf

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -115,7 +115,7 @@ router.post('/login', loginLimiter, async (req, res) => {
     const token = generateToken(user._id);
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
     });
